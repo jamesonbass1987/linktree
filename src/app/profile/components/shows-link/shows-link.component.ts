58d87a2e9a28a828e78d6f49862e0abc5d73e23d
Jameson Bass <jamesonbass@gmail.com>
2022-03-19 12:51:25 -0700

    feat: build out shows component

--- src/app/profile/components/shows-link/shows-link.component.ts
+++ src/app/profile/components/shows-link/shows-link.component.ts
@@ -1,24 +1,27 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { Link, ShowsLink, Venue } from 'src/app/core/models/link.model';
 
 @Component({
   selector: 'app-shows-link',
   templateUrl: './shows-link.component.html',
-  styleUrls: ['./shows-link.component.scss']
+  styleUrls: ['./shows-link.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ShowsLinkComponent implements OnInit {
-
+export class ShowsLinkComponent {
   private _link!: Link;
   @Input() set link(link: Link) {
     this._link = link;
     this.venues = (link as ShowsLink).venues;
   };
+  get link(): Link {
+    return this._link;
+  }
 
   public venues: Venue[] = [];
 
   constructor() { }
 
-  ngOnInit(): void {
+  onVenueItemClicked(item: Venue): void {
+    window.open(item.url)
   }
-
 }
